0a20bea58aaf4aaa55d4865c9b8b5b324de0e5fc
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-06 22:15:23 +0900

    Initial Commit

---
+++ Swift6WKWebView1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6WKWebView1
+//
+//  Created by Takuya Ando on 2020/12/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
